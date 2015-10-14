8714f4ab2fa02165ee6ba8227fe76c078b3678d4
TropicSapling <tropical.productions.scratch@gmail.com>
2015-10-14 21:59:31 +0200

    Buggy Update
    Just a quick update. Contains some bugs though :/

--- Batch/compile.bat
+++ Batch/compile.bat
@@ -1,5 +1,4 @@
 @echo off
 cd ..
 python "Python/compiler.py"
-echo Compilation done!
 PAUSE
--- Python/compiler.py
+++ Python/compiler.py
@@ -1,1 +1,26 @@
-print("test");
+def getFileLen(dir):
+	i = 0
+	for l in dir:
+		i += 1
+	return i
+
+path = input("Specify the location of the file you want to compile:")
+print("Compiling...")
+if(len(path) < 1):
+	print("ERROR #0: Invalid file location. Seriosly, you can't just write nothing -.-")
+elif(not(path.endswith(".pbtl"))):
+	print("ERROR #1: Invalid file extension. Must be '.pbtl'.")
+else:
+	destination = open(path[:-4] + "py", "w")
+	source = open(path, "r")
+	lineNumber = 0
+	for line in source:
+		if(line.endswith("2")):
+			destination.write("here")
+		else:
+			destination.write(line)
+		lineNumber += 1
+		print("%.2f%s" % (round((lineNumber / getFileLen(source))*100, 2), "%"))
+	source.close()
+	destination.close()
+	print("Compilation done.")
