bcb18f6672f0322dae8e5105e891d7cb6ce15e3f
TropicSapling <tropical.productions.scratch@gmail.com>
2015-10-20 20:11:22 +0200

    Added print as list
    Added basic print thing but for now you can only print things as a list
    xD
    It's not too useful right now because lists haven't been added yet, but
    when lists have been added you could print it as a list and then it will
    look a bit more fancy, instead of converting it into a string without
    spaces and print it or just printing it raw :)

--- Python/compiler.py
+++ Python/compiler.py
@@ -1,8 +1,9 @@
 def getFileLen(dir):
-	i = 0
-	for l in dir:
-		i += 1
-	return i
+	o = sum(1 for _ in dir)
+	if(str(o).endswith("0", 0, 0)):
+		return o[1:] # Don't even know why this works but it works so...
+	else:
+		return o
 
 path = input("Specify the location of the file you want to compile:")
 print("Compiling...")
@@ -13,14 +14,36 @@ elif(not(path.endswith(".pbtl"))):
 else:
 	destination = open(path[:-4] + "py", "w")
 	source = open(path, "r")
+	fileLength = getFileLen(source)
+	source.close()
+	source = open(path, "r") # Yes this is necessary because of the garbage collector...
 	lineNumber = 0
+	charNumber = 0
+	foundPrint = False
+	lineLi = []
+	tmpLi = []
+	liValue = 0
 	for line in source:
-		if(line.endswith("2")):
-			destination.write("here")
-		else:
-			destination.write(line)
+		for char in line:
+			lineLi.append(char)
+			if(("".join(lineLi)).endswith("print ") or foundPrint):
+				if(not foundPrint):
+					liValue += 1
+					foundPrint = True
+				else:
+					tmpLi.append(char)
+					if(("".join(lineLi)).endswith(" as list")):
+						tmpLi = tmpLi[:-8]
+						destination.write("print(\", \".join({}))".format(tmpLi))
+						tmpLi = []
+						foundPrint = False
+			charNumber += 1
+		if(lineNumber+1 != fileLength):
+			destination.write("\n")
+		charNumber = 0
+		lineLi = []
 		lineNumber += 1
-		print("%.2f%s" % (round((lineNumber / getFileLen(source))*100, 2), "%"))
+		print("%.2f%s" % (round((lineNumber / fileLength)*100, 2), "%"))
 	source.close()
 	destination.close()
 	print("Compilation done.")
