0eb874683eed041ba3cc5af324fb91ae1aeb2da6
TropicSapling <tropical.productions.scratch@gmail.com>
2015-10-21 20:36:23 +0200

    Added "nlist" parameter and normal printing
    You can now print normally as a string using no parameters or print
    everything as a list seperated by newlines using the "nlist" parameter
    :)
    
    Also tried to add "wait n secs/seconds" but it doesn't work yet because
    I need to import time in Python first, which sadly only is possible by
    rewriting the whole compiler :/
    But I'll try doing that in the next commit :)

--- Python/compiler.py
+++ Python/compiler.py
@@ -1,3 +1,5 @@
+import time
+
 def getFileLen(dir):
 	o = sum(1 for _ in dir)
 	if(str(o).endswith("0", 0, 0)):
@@ -5,7 +7,7 @@ def getFileLen(dir):
 	else:
 		return o
 
-path = input("Specify the location of the file you want to compile:")
+path = input("Specify the location of the file you want to compile: ")
 print("Compiling...")
 if(len(path) < 1):
 	print("ERROR #0: Invalid file location. Seriosly, you can't just write nothing -.-")
@@ -20,6 +22,7 @@ else:
 	lineNumber = 0
 	charNumber = 0
 	foundPrint = False
+	foundWait = False
 	lineLi = []
 	tmpLi = []
 	liValue = 0
@@ -32,11 +35,35 @@ else:
 					foundPrint = True
 				else:
 					tmpLi.append(char)
-					if(("".join(lineLi)).endswith(" as list")):
-						tmpLi = tmpLi[:-8]
+					if(("".join(lineLi)).endswith("; as list")):
+						tmpLi = tmpLi[:-9]
 						destination.write("print(\", \".join({}))".format(tmpLi))
 						tmpLi = []
 						foundPrint = False
+					elif(("".join(lineLi)).endswith("; as nlist")):
+						tmpLi = tmpLi[:-11]
+						destination.write("print(\"\\n\".join({}))".format(tmpLi))
+						tmpLi = []
+						foundPrint = False
+					elif(charNumber+1 == len(line)):
+						tmpLi = tmpLi[:-1]
+						destination.write("print(\"{}\")".format("".join(tmpLi)))
+						tmpLi = []
+						foundPrint = False
+			elif(("".join(lineLi)).endswith("wait ") or foundWait):
+				if(not foundWait):
+					liValue += 1
+					foundWait = True
+				else:
+					tmpLi.append(char)
+					if((("".join(lineLi)).endswith(" secs")) or (("".join(lineLi)).endswith(" seconds"))):
+						if(("".join(lineLi)).endswith(" secs")):
+							tmpLi = tmpLi[:-5]
+						else:
+							tmpLi = tmpLi[:-8]
+						destination.write("time.sleep({})".format("".join(tmpLi)))
+						tmpLi = []
+						foundWait = False
 			charNumber += 1
 		if(lineNumber+1 != fileLength):
 			destination.write("\n")
