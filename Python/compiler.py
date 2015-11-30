0417030890e227f7c56f2eb31f8b85c1a744c7dc
ninjagolloyd <ninjagolloyd14@gmail.com>
2015-11-30 06:35:03 -0500

    Update compiler
    Switch .pseudo to .pb

--- Python/compiler.py
+++ Python/compiler.py
@@ -13,8 +13,8 @@ useDebug = input("Print debug messages? (Yes / No)\n").upper()
 print("Preparing...")
 if(len(path) < 1):
 	print("ERROR #0: Invalid file location. Seriosly, you can't just write nothing -.-")
-elif(not(path.endswith(".pbtl"))&&(not(path.endswith(".pseudo"))):
-	print("ERROR #1: Invalid file extension. Must be '.pbtl' or '.pseudo'.")
+elif(not(path.endswith(".pbtl"))&&(not(path.endswith(".pb"))):
+	print("ERROR #1: Invalid file extension. Must be '.pbtl' or '.pb'.")
 else:
 	with open(path) as f:
 		fileLi = f.read()
